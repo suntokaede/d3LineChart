a43bd12021120d8d0e2c6d49c0f8c32258c96b15
suntokaede <suntokaede@gmail.com>
2016-01-26 18:39:56 +0900

    update

--- d3LineChart.ts
+++ d3LineChart.ts
@@ -1,44 +1,75 @@
-/// <reference path="../typings/d3/d3.d.ts"/>
-var d3LineChart = (function () {
-    function d3LineChart(url, args) {
-        this.defaultOptions = {
-            margin: {
-                top: 20,
-                right: 30,
-                bottom: 30,
-                left: 60
-            },
-            height: 500,
-            selectWrapperId: "selectWrapper",
-            graphWrapperId: "graphWrapper",
-            charset: "Shift_JIS",
-            timeFormat: "%Y/%m/%d",
-            xAxisFormat: "%m月%d日(%a)",
-            xAxisTicks: null,
-            yDomain: [null, null],
-            mouseOverTransitionTime: 500,
-            mouseOutTransitionTime: 500,
-            locale: {
-                "decimal": ".",
-                "thousands": ",",
-                "grouping": [3],
-                "currency": ["", "円"],
-                "dateTime": "%a %b %e %X %Y",
-                "date": "%Y/%m/%d",
-                "time": "%H:%M:%S",
-                "periods": ["AM", "PM"],
-                "days": ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
-                "shortDays": ["日", "月", "火", "水", "木", "金", "土"],
-                "months": ["睦月", "如月", "弥生", "卯月", "皐月", "水無月", "文月", "葉月", "長月", "神無月", "霜月", "師走"],
-                "shortMonths": ["01月", "02月", "03月", "04月", "05月", "06月", "07月", "08月", "09月", "10月", "11月", "12月"]
-            }
-        };
+﻿/// <reference path="../typings/d3/d3.d.ts"/>
+
+class d3LineChart {
+    private url: string;
+    private options: { margin: { top: number; right: number; bottom: number; left: number; }; height: number; selectWrapperId: string; graphWrapperId: number; charset: string; timeFormat: string; xAxisFormat: string; xAxisTicks: number; yDomain: any; mouseOverTransitionTime: number; mouseOutTransitionTime: number; locale: any; };
+    private args: any;
+    private defaultOptions: {} = {
+        margin: {
+            top: 20,
+            right: 30,
+            bottom: 30,
+            left: 60
+        },
+        height: 500,
+        selectWrapperId: "selectWrapper",
+        graphWrapperId: "graphWrapper",
+        charset: "Shift_JIS",
+        timeFormat: "%Y/%m/%d",
+        xAxisFormat: "%m月%d日(%a)",
+        xAxisTicks: null,
+        yDomain: [null,null],
+        mouseOverTransitionTime: 500,
+        mouseOutTransitionTime: 500,
+        locale: {
+            "decimal": ".",
+            "thousands": ",",
+            "grouping": [3],
+            "currency": ["", "円"],
+            "dateTime": "%a %b %e %X %Y",
+            "date": "%Y/%m/%d",
+            "time": "%H:%M:%S",
+            "periods": ["AM", "PM"],
+            "days": ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"],
+            "shortDays": ["日", "月", "火", "水", "木", "金", "土"],
+            "months": ["睦月", "如月", "弥生", "卯月", "皐月", "水無月", "文月", "葉月", "長月", "神無月", "霜月", "師走"],
+            "shortMonths": ["01月", "02月", "03月", "04月", "05月", "06月", "07月", "08月", "09月", "10月", "11月", "12月"]
+        }
+    };
+    private locale: any;
+    private separator: string;
+    private fileFormat: string;
+    private selectWrapper: HTMLElement;
+    private d3selectWrapper: any;
+    private d3graphWrapper: any;
+    private parseDay: any;
+    private parseDate: any;
+    private loadFile: any;
+    private data: any;
+    private keys: any;
+    private select: any;
+    private width: number;
+    private height: number;
+    private x: any;
+    private y: any;
+    private xAxis: any;
+    private yAxis: any;
+    private svg: any;
+    private svgEle: any;
+    private toolTip: any;
+    private legend: any;
+    private colorCategoryScale: any;
+    private xAxisTicks: any;
+    private yDomain: any;
+
+    constructor(url: string, args?: {}) {
         this.url = url;
         this.args = args;
         this.main();
     }
+
     //初期化
-    d3LineChart.prototype.init = function () {
+    init() {
         this.options = this.updateObj(this.defaultOptions, this.args);
         this.selectWrapper = document.getElementById(this.options.selectWrapperId);
         this.d3selectWrapper = d3.select("#" + this.options.selectWrapperId);
@@ -69,19 +100,20 @@ var d3LineChart = (function () {
         this.yAxis = d3.svg.axis().innerTickSize(-this.width).outerTickSize(0).tickPadding(10);
         this.toolTip = this.d3graphWrapper.append("div").attr("class", "d3_tooltip");
         this.legend = this.d3graphWrapper.append("div").attr("class", "d3_legend")
-            .style({ top: (this.options.margin.top + 10) + "px", right: (this.options.margin.right + 10) + "px" });
+                      .style({ top: `${this.options.margin.top + 10}px`, right: `${this.options.margin.right + 10}px` });
         this.parseDay = d3.time.format(this.options.xAxisFormat);
         this.parseDate = d3.time.format(this.options.timeFormat).parse;
         this.locale = d3.locale(this.options.locale);
         this.colorCategoryScale = d3.scale.category10();
         this.loadFile = d3.dsv(this.separator, "text/" + this.fileFormat + "; charset=" + this.options.charset);
-    };
-    d3LineChart.prototype.line = function (key) {
-        var _this = this;
-        return d3.svg.line().x(function (d) { return _this.x(d.__x); }).y(function (d) { return _this.y(d[("__" + key)]); });
-    };
+    }
+
+    line(key: any) {
+        return d3.svg.line().x((d: any) => this.x(d.__x)).y((d: any) => this.y(d[`__${key}`]));
+    }
+
     // オブジェクトをオブジェクトで上書き（masterが上書きされる側,overwriterが上書きする側）
-    d3LineChart.prototype.updateObj = function (master, overwriter) {
+    updateObj(master, overwriter) {
         for (var l in overwriter) {
             // Object, Array判定
             if (typeof overwriter[l] === "object") {
@@ -89,45 +121,49 @@ var d3LineChart = (function () {
                     master[l] = {};
                 }
                 this.updateObj(master[l], overwriter[l]);
-            }
-            else {
+            //その他
+            } else {
                 master[l] = overwriter[l];
             }
         }
         return master;
-    };
+    }
+
     //ファイルの拡張子を取得します
-    d3LineChart.prototype.fetchExtension = function () {
-        if (!this.url)
-            return;
+    fetchExtension() {
+        if (!this.url) return;
         var str = this.url.split(/\.(?=[^.]+$)/);
         var strLength = str.length;
         return str[strLength - 1];
-    };
+    }
+
     //チェックが入っているチェックボックスのキー一覧を返します
-    d3LineChart.prototype.listCheckedboxKeys = function () {
-        var checkboxes = this.d3selectWrapper.selectAll("label").selectAll("input[type='checkbox']"), ret = [];
-        checkboxes.forEach(function (e) {
+    listCheckedboxKeys() {
+        var checkboxes: any = this.d3selectWrapper.selectAll("label").selectAll("input[type='checkbox']"),
+            ret = [];
+        checkboxes.forEach((e) => {
             if (e[0].checked) {
                 ret.push(e[0].dataset.key);
             }
         });
         return ret;
-    };
+    }
+
     //表示されているすべてのデータが枠に収まるドメインを生成します
-    d3LineChart.prototype.makeDomainFromCheckedItem = function () {
-        var _this = this;
-        var ret = [0, 0], tmpMax = this.options.yDomain[1] != null ? [this.options.yDomain[1]] : [], tmpMin = this.options.yDomain[0] != null ? [this.options.yDomain[0]] : [];
-        this.listCheckedboxKeys().forEach(function (key) {
-            tmpMax.push(d3.max(_this.data, function (d) { return d[("__" + key)]; }));
-            tmpMin.push(d3.min(_this.data, function (d) { return d[("__" + key)]; }));
+    makeDomainFromCheckedItem() {
+        var ret = [0, 0],
+            tmpMax = this.options.yDomain[1] != null ? [this.options.yDomain[1]] : [],
+            tmpMin = this.options.yDomain[0] != null ? [this.options.yDomain[0]] : [];
+        this.listCheckedboxKeys().forEach((key) => {
+                tmpMax.push(d3.max(this.data, (d: any) => d[`__${key}`]));
+                tmpMin.push(d3.min(this.data, (d: any) => d[`__${key}`]));
         });
         ret = [Math.min.apply(null, tmpMin), Math.max.apply(null, tmpMax)];
         return ret;
-    };
+    }
+
     // CSV or TSVの読み込み
-    d3LineChart.prototype.load = function (data) {
-        var _this = this;
+    load(data?) {
         if (data) {
             this.data = data;
             //ヘッダーのキーを取得
@@ -135,56 +171,57 @@ var d3LineChart = (function () {
             //データのパース、チェックボックスの作成、イベントリスナーの登録
             this.parseAllData();
             this.createCheckbox();
-            window.addEventListener("resize", function () { _this.update.call(_this); }, false);
+            window.addEventListener("resize", () => {this.update.call(this);}, false);
             //ここでreturnしないとまたloadFileが呼ばれて無限ループします
             return;
         }
         //loadFile(d3.dsv)が非同期メソッドの為、読み込みが終了したときにload関数を再度呼び出すようにしています
-        this.loadFile(this.url, function (error, data) {
+        this.loadFile(this.url, (error, data) => {
             if (error != null) {
                 console.error(error);
                 return;
             }
             if (data) {
-                _this.load(data);
+                this.load(data);
             }
         });
-    };
+    }
+
     //チェックボックスを作成します
-    d3LineChart.prototype.createCheckbox = function () {
-        var _this = this;
+    createCheckbox() {
         for (var i = 1, l = this.keys.length; i < l; i++) {
-            var label = this.d3selectWrapper.append("label").attr("data-key", this.keys[i]).style("display", "block").style("margin-bottom", "10px");
-            label.append("input").attr("type", "checkbox").attr("data-key", this.keys[i]).style("display", "none")
-                .on("change", function () { _this.update.call(_this); });
-            label.append("span").attr("style", "display: inline-block; margin-right: 5px; border-radius: 50%; width: 16px; height: 16px; background-color:" + this.colorCategoryScale("__" + this.keys[i]));
-            label.append("text").text(this.keys[i]);
+            var label = this.d3selectWrapper.append("label").attr("data-key", this.keys[i]).style("display","block").style("margin-bottom", "10px");
+                label.append("input").attr("type", "checkbox").attr("data-key", this.keys[i]).style("display","none")
+                .on("change", () => { this.update.call(this); });
+                label.append("span").attr("style", "display: inline-block; margin-right: 5px; border-radius: 50%; width: 16px; height: 16px; background-color:" + this.colorCategoryScale(`__${this.keys[i]}`));
+                label.append("text").text(this.keys[i]);
         }
-    };
+    }
+
     //凡例のアクティブな項目をハイライトします
-    d3LineChart.prototype.highlightActiveLegend = function () {
-        var _this = this;
+    highlightActiveLegend() {
         this.d3selectWrapper.selectAll("label").style("background-color", "transparent");
-        this.listCheckedboxKeys().forEach(function (key) {
-            _this.d3selectWrapper.selectAll("label[data-key='" + key + "']").style("background-color", "#ceede5");
+        this.listCheckedboxKeys().forEach((key) => {
+            this.d3selectWrapper.selectAll("label[data-key='" + key + "']").style("background-color", "#ceede5");
         });
-    };
+    }
+
     //全てのデータをパースします
-    d3LineChart.prototype.parseAllData = function () {
-        var _this = this;
-        this.data.forEach(function (d) {
-            d["__x"] = _this.parseDate(d[_this.keys[0]]);
-            for (var i = 1, l = _this.keys.length; i < l; i++) {
-                d[("__" + _this.keys[i])] = +d[_this.keys[i]];
+    parseAllData() {
+        this.data.forEach((d) => {
+            d["__x"] = this.parseDate(d[this.keys[0]]);
+            for (var i = 1, l = this.keys.length; i < l; i++) {
+                d[`__${this.keys[i]}`] = +d[this.keys[i]];
             }
         });
-    };
+    }
+
     //ステータスの更新
-    d3LineChart.prototype.update = function () {
+    update() {
         //値の変更の影響を受けるメソッドを再度実行
         this.width = this.d3graphWrapper.node().getBoundingClientRect().width - this.options.margin.left - this.options.margin.right;
         this.height = this.options.height - this.options.margin.top - this.options.margin.bottom;
-        this.x = d3.time.scale().domain(d3.extent(this.data, function (d) { return d.__x; })).range([0, this.width]);
+        this.x = d3.time.scale().domain(d3.extent(this.data, (d: any) => d.__x)).range([0, this.width]);
         this.y = d3.scale.linear().domain(this.makeDomainFromCheckedItem()).range([this.height, 0]);
         this.xAxisTicks = Math.min(this.options.xAxisTicks | this.xAxis.ticks()[0], this.data.length);
         this.xAxis.scale(this.x).orient("bottom").innerTickSize(-this.height).tickFormat(this.locale.timeFormat(this.options.xAxisFormat)).ticks(this.xAxisTicks);
@@ -199,24 +236,26 @@ var d3LineChart = (function () {
         this.clear();
         //draw呼び出し
         this.draw();
-    };
+    }
+
     //SVGをクリア
-    d3LineChart.prototype.clear = function () {
+    clear() {
         this.d3graphWrapper.selectAll("path").remove();
         this.d3graphWrapper.selectAll("circle").remove();
         this.d3graphWrapper.selectAll("g[class='y axis']").remove();
         this.d3graphWrapper.selectAll("g[class='x axis']").remove();
         this.legend.selectAll("label").remove();
-    };
+    }
+
     //描写
-    d3LineChart.prototype.draw = function () {
-        var _this = this;
+    draw() {
+        
         //x軸
         this.svg.append("g")
             .attr({
-            class: "x axis",
-            transform: "translate(0," + this.height + ")"
-        })
+                class: "x axis",
+                transform: "translate(0," + this.height + ")"
+            })
             .call(this.xAxis);
         //y軸
         this.svg.append("g")
@@ -225,50 +264,52 @@ var d3LineChart = (function () {
             .append("text")
             .attr({ transform: "rotate(-90)", y: 6, dy: ".71em" })
             .style("text-anchor", "end");
+
         //表示する項目のパス、点と凡例を描写
-        this.listCheckedboxKeys().forEach(function (key) {
+        this.listCheckedboxKeys().forEach((key) => {
             //パス
-            _this.svg.append("path")
-                .datum(_this.data)
-                .attr({ class: "line", d: _this.line(key), stroke: function () { return _this.colorCategoryScale("__" + key); } });
+            this.svg.append("path")
+                .datum(this.data)
+                .attr({ class: "line", d: this.line(key), stroke: () => this.colorCategoryScale(`__${key}`) });
             //点とツールチップス
-            _this.svg.selectAll("dot")
-                .data(_this.data)
+            this.svg.selectAll("dot")
+                .data(this.data)
                 .enter()
                 .append("circle")
-                .on("mouseover", function (d) {
-                _this.toolTip.transition()
-                    .duration(_this.options.mouseOverTransitionTime)
-                    .style("opacity", 1);
-                _this.toolTip.html(d[("__" + key)] + "<br/>" + d.__x.toLocaleDateString())
-                    .style("left", _this.x(d.__x) + 60 + "px")
-                    .style("top", _this.y(d[("__" + key)]) - 35 + "px");
-            })
-                .on("mouseout", function () {
-                _this.toolTip.transition()
-                    .duration(_this.options.mouseOutTransitionTime)
-                    .style("opacity", 0);
-            })
+                .on("mouseover", (d) => {
+                    this.toolTip.transition()
+                        .duration(this.options.mouseOverTransitionTime)
+                        .style("opacity", 1);
+                    this.toolTip.html(d[`__${key}`] + "<br/>" + d.__x.toLocaleDateString())
+                        .style("left", this.x(d.__x) + 60 + "px")
+                        .style("top", this.y(d[`__${key}`]) - 35 + "px");
+                })
+                .on("mouseout", () => {
+                    this.toolTip.transition()
+                        .duration(this.options.mouseOutTransitionTime)
+                        .style("opacity", 0);
+                })
                 .attr({
-                cx: function (d) { return _this.x(d.__x); },
-                cy: function (d) { return _this.y(d[("__" + key)]); },
-                r: 0,
-                stroke: "#black",
-                fill: function () { return _this.colorCategoryScale("__" + key); }
-            })
+                    cx: (d) => this.x(d.__x),
+                    cy: (d) => this.y(d[`__${key}`]),
+                    r: 0,
+                    stroke: "#black",
+                    fill: () => this.colorCategoryScale(`__${key}`)
+                })
                 .attr("stroke-width", "1px")
                 .transition()
                 .duration(1000)
                 .attr("r", 3.5);
+
             //凡例
-            _this.legend.append("label").style("margin-bottom", "10px").style({ display: "block" })
-                .html("<div style='display: inline-block; border-radius: 50%; width :14px; height:14px; background-color:" + _this.colorCategoryScale("__" + key) + "'></div> " + key + "<br>");
+            this.legend.append("label").style("margin-bottom", "10px").style({ display: "block" })
+                       .html("<div style='display: inline-block; border-radius: 50%; width :14px; height:14px; background-color:" + this.colorCategoryScale(`__${key}`) + "'></div> " + key + "<br>");
         });
-    };
-    d3LineChart.prototype.main = function () {
+    }
+
+    main() {
         this.init();
         this.load();
-    };
-    return d3LineChart;
-})();
-//# sourceMappingURL=d3LineChart.js.map
+    }
+
+}
